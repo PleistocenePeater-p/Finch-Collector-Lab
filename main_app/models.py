ca0658775110873348e182d7c7ef128f62e5a2c5
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-08-17 10:19:44 -0700

    Lab 3 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Finch(models.Model):
@@ -6,3 +7,10 @@ class Finch(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     progeny = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+  # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch
 
 from .models import Finch
 # Create your views here.
@@ -17,3 +20,15 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'progeny']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
