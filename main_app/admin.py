6017dfa1a8e349a9f82d89f71ba5a1d856202a33
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-08-17 16:24:01 -0700

    Lab 4 Done

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Finch
+from .models import Finch, Feeding
 
 #allows full CRUDding in our admin app for finch table in db
 admin.site.register(Finch)
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import Finch
+from .forms import FeedingForm
 
 from .models import Finch
 # Create your views here.
@@ -19,7 +20,11 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch,
+        'feeding_form': feeding_form,
+        })
 
 class FinchCreate(CreateView):
     model = Finch
@@ -32,3 +37,20 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches'
+
+
+def add_feeding(request, finch_id):
+  print(finch_id)
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
