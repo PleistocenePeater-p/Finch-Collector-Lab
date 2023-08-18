a68a626ec06798a2342a8befc855a322e12217b4
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-08-17 19:04:09 -0700

    Lab 5 Done

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Finch, Feeding
+from .models import Finch, Feeding, Toy
 
 #allows full CRUDding in our admin app for finch table in db
 admin.site.register(Finch)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/models.py
+++ main_app/models.py
@@ -9,18 +9,33 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     progeny = models.IntegerField()
+    #M:M
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.id})'
     
   # Add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self):
+       return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
